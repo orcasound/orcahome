2c5b1ac5ca2cd33fe765b8aac11441242eed9dbf
isabella macchiavello <69311863+imacchiavello@users.noreply.github.com>
2021-08-26 10:55:51 -0500

    updated navbar added bootstrap
    declare new variables so it will render react-bootsrap (line 17)

--- components/Nav.js
+++ components/Nav.js
@@ -1,120 +1,56 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
-
+import Image from 'next/image'
 import orcasoundlogo from '../images/orcasoundlogo.png'
 import navStyles from '../styles/Nav.module.css'
+import Link from 'next/link'
+import { withRouter } from 'next/router'
+import classNames from 'classnames'
 
-export default function Nav({ children }) {
-  const [isOpen, setIsOpen] = useState(false)
-  const openMenu = () => setIsOpen(!isOpen)
-  return (
-    <>
-      <header className={navStyles.header}>
-        <nav className={navStyles.navbar}>
-          <Link href="/">
-            <a className={navStyles.navlogo}>
-              <Image src={orcasoundlogo} width={90} height={70} />
-            </a>
-          </Link>
-          <ul
-            className={
-              isOpen === false
-                ? navStyles.navmenu
-                : navStyles.navmenu + ' ' + navStyles.active
-            }
-          >
-            <li className={navStyles.navitem}>
-              <Link href="/about">
-                <a
-                  className={
-                    isOpen === false
-                      ? navStyles.navlink
-                      : navStyles.navlink + ' ' + navStyles.active
-                  }
-                  onClick={openMenu}
-                >
-                  About
-                </a>
-              </Link>
-            </li>
-            <li className={navStyles.navitem}>
-              <Link href="/learn">
-                <a
-                  className={
-                    isOpen === false
-                      ? navStyles.navlink
-                      : navStyles.navlink + ' ' + navStyles.active
-                  }
-                  onClick={openMenu}
-                >
-                  learn
-                </a>
-              </Link>
-            </li>
+import { Container, Navbar, Nav , NavDropdown} from 'react-bootstrap'
 
-            <li className={navStyles.navitem}>
-              <Link href="/getinvolved">
-                <a
-                  className={
-                    isOpen === false
-                      ? navStyles.navlink
-                      : navStyles.navlink + ' ' + navStyles.active
-                  }
-                  onClick={openMenu}
-                >
-                  get involved
-                </a>
-              </Link>
-            </li>
+<style>
 
-            <li className={navStyles.navitem}>
-              <Link href="/https://live.orcasound.net/">
-                <a
-                  className={
-                    isOpen === false
-                      ? navStyles.navlink
-                      : navStyles.navlink + ' ' + navStyles.active
-                  }
-                  onClick={openMenu}
-                >
-                  Listen
-                </a>
-              </Link>
-            </li>
-            <li className={navStyles.navitem}>
-              <Link href="/">
-                <Button
-                  className={
-                    isOpen === false
-                      ? navStyles.navlink
-                      : navStyles.navlink + ' ' + navStyles.active
-                  }
-                  onClick={openMenu}
-                >
-                  {' '}
-                  Notify me{' '}
-                </Button>
-              </Link>
-            </li>
-          </ul>
-          <button
-            className={
-              isOpen === false
-                ? navStyles.hamburger
-                : navStyles.hamburger + ' ' + navStyles.active
-            }
-            onClick={openMenu}
-          >
-            <span className={navStyles.bar}></span>
-            <span className={navStyles.bar}></span>
-            <span className={navStyles.bar}></span>
-          </button>
-        </nav>
-      </header>
-    </>
-  )
+</style>
+export const NavLink = (props) => {
+
+  let className = classNames({
+    "nav-link": true,
+    "is-active": props.pathname
+  })
+  return <Link href={props.path}><a className={className}>{props.label}</a></Link>
+
+
 }
+
+const Header = ({ router: { pathname } }) => (
+  <div >
+  <header className={navStyles.navigation}>
+  
+ <div>
+ 
+
+  <Navbar  variant="dark"   bg="dark"  expand="lg"  fixed="top">
+  
+  <Navbar.Brand href="/" > <Image passhref src={orcasoundlogo} width={90} height={50} path="/"  /> </Navbar.Brand>
+    <Navbar.Toggle  className="justify-content-end" aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+      <NavLink className="justify-content-end" path="/about" label="About Us" pathname/>
+              <NavLink  className="justify-content-end"  path="/learn" label="Learn" pathname/>
+              <NavLink  className="justify-content-end" path="/getinvolved" label="Get Involved" pathname/>
+           <NavLink className="justify-content-end" path='/http://live.orcasound.net/' label="Listen" pathname/> 
+              
+      </Nav>
+      <Button className="justify-content-end">Notify Me</Button>
+    </Navbar.Collapse>
+ 
+</Navbar>
+</div>
+
+   
+  </header>
+  </div>
+)
+
+export default withRouter(Header)
