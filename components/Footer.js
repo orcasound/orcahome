6c98eb67a03f2a20ebdaf00dc826449a71c81716
isabella macchiavello <69311863+imacchiavello@users.noreply.github.com>
2021-08-26 11:22:49 -0500

    Created Footer
    It has some sort of bug tho cause it doesn't render in all pages automatically

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,79 @@
-import FacebookIcon from '@material-ui/icons/Facebook'
-import InstagramIcon from '@material-ui/icons/Instagram'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import RedditIcon from '@material-ui/icons/Reddit'
-import TwitterIcon from '@material-ui/icons/Twitter'
+
+import footerStyles from '../styles/Footer.module.css'
 import Link from 'next/link'
-import Button from 'react-bootstrap/Button'
+import { withRouter } from 'next/router'
+import classNames from 'classnames'
+
+import { Container, Navbar, Nav , NavDropdown} from 'react-bootstrap'
+
+<style>
+
+</style>
+export const NavLink = (props) => {
+
+  let className = classNames({
+    "nav-link": true,
+    "is-active": props.pathname
+  })
+  return <Link href={props.path}><a className={className}>{props.label}</a></Link>
+
+
+}
+
+const Footer = ({ router: { pathname } }) => (
+  <div className={footerStyles.footer}>
+  <header className={footerStyles.foot}>
+  
+ 
+  <Nav className="justify-content-end" defaultActiveKey="/home" as="ul"  bg="dark" expand="lg">
+  <Nav.Item as="li">
+    <Nav.Link href="/donate">Donate</Nav.Link>
+  </Nav.Item>
+  <Nav.Item as="li">
+    <Nav.Link eventKey="link-1"><a href="#" class='fa fa-facebook'> </a></Nav.Link>
+  </Nav.Item>
+
+  <Nav.Item as="li">
+    <Nav.Link eventKey="link-1">  <a href="#" class="fa fa-instagram"> </a></Nav.Link>
+  </Nav.Item>
+
+  <Nav.Item as="li">
+    <Nav.Link eventKey="link-1"> 
+  <a href="#" class="fa fa-linkedin"></a></Nav.Link>
+  </Nav.Item>
+
+
+  <Nav.Item as="li">
+    <Nav.Link eventKey="link-1">  <a href="#" class="fa fa-youtube"></a></Nav.Link>
+  </Nav.Item>
+
+
+  <Nav.Item as="li">
+    <Nav.Link eventKey="link-1">  
+  <a href="#" class="fa fa-reddit"></a></Nav.Link>
+  </Nav.Item>
+
+  <Nav.Item as="li">
+    <Nav.Link eventKey="link-1">   <a href="#" class="fa fa-twitter"></a>
+ </Nav.Link>
+  </Nav.Item>
+
+
+  <Nav.Item as="li">
+    <Nav.Link eventKey="link-1">   <a href="#" class="fa-solid fa-bell"></a>
+ </Nav.Link>
+  </Nav.Item>
+
+
+
+
+
+</Nav>
+
+       
+   
+  </header>
+  </div>
+)
+
+export default withRouter(Footer)
