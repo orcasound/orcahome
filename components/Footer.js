4fe4bd59219223a69d8105ef6471c65646018428
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2021-12-18 15:55:38 +0530

    update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,6 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import FooterStyles from '../styles/Footer.module.css'
 import Link from 'next/link'
 import YouTubeIcon from '@mui/icons-material/YouTube'
@@ -15,67 +13,87 @@ import NotificationsIcon from '@mui/icons-material/Notifications'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import RedditIcon from '@mui/icons-material/Reddit'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import useCheckMobileScreen from '../utils/useCheckMobileScreen'
 
-export default function ButtonAppBar() {
+export default function Footer() {
+  const isMobile = useCheckMobileScreen()
+
+  const iconContainer = (
+    <div className={FooterStyles.iconContainer}>
+      <a
+        href="https://www.facebook.com/OrcasoundApp/"
+        className={FooterStyles.icon}
+      >
+        <FacebookIcon fontSize="large" />
+      </a>
+      <a
+        href="https://www.linkedin.com/company/75491849/admin/"
+        className={FooterStyles.icon}
+      >
+        <LinkedInIcon fontSize="large" />
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UC7b3tOVQg8_fzaZBj4NoAEg"
+        className={FooterStyles.icon}
+      >
+        <YouTubeIcon fontSize="large" />
+      </a>
+      <a href="https://twitter.com/OrcasoundApp" className={FooterStyles.icon}>
+        <TwitterIcon fontSize="large" />
+      </a>
+      <a
+        href="https://www.instagram.com/orcasoundapp/"
+        className={FooterStyles.icon}
+      >
+        <InstagramIcon fontSize="large" />
+      </a>
+    </div>
+  )
+  const sendFeedbackLink = (
+    <Link href="/">
+      <Typography variant="h6" component="a" className={FooterStyles.typo}>
+        Send Feedback
+      </Typography>
+    </Link>
+  )
+  const blogLink = (
+    <Link href="/">
+      <Typography variant="h6" component="a" className={FooterStyles.typo}>
+        Blog
+      </Typography>
+    </Link>
+  )
+
+  if (isMobile)
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" className={FooterStyles.footer_mobile}>
+          <div>{sendFeedbackLink}</div>
+          <div>{blogLink}</div>
+          <div>
+            <Link href="/">
+              <Typography
+                variant="h6"
+                component="a"
+                className={FooterStyles.typo}
+              >
+                Donate
+              </Typography>
+            </Link>
+          </div>
+          {iconContainer}
+        </AppBar>
+      </Box>
+    )
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" className={FooterStyles.appbar}>
+      <AppBar position="static" className={FooterStyles.footer}>
         <Toolbar>
           <div className={FooterStyles.div}></div>
-          <Link href="/">
-            <Typography
-              variant="h6"
-              component="a"
-              className={FooterStyles.typo}
-            >
-              Send Feedback
-            </Typography>
-          </Link>
-          <Link href="/">
-            <Typography
-              variant="h6"
-              component="a"
-              // sx={{ color: 'white' }}
-              className={FooterStyles.typo}
-            >
-              Blog
-            </Typography>
-          </Link>
-          <div className={FooterStyles.iconContainer}>
-            <a
-              href="https://www.facebook.com/OrcasoundApp/"
-              className={FooterStyles.icon}
-            >
-              <FacebookIcon fontSize="large" />
-            </a>
-            <a href="/">
-              <RedditIcon fontSize="large" className={FooterStyles.icon} />
-            </a>
-            <a
-              href="https://www.linkedin.com/company/75491849/admin/"
-              className={FooterStyles.icon}
-            >
-              <LinkedInIcon fontSize="large" />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UC7b3tOVQg8_fzaZBj4NoAEg"
-              className={FooterStyles.icon}
-            >
-              <YouTubeIcon fontSize="large" />
-            </a>
-            <a
-              href="https://twitter.com/OrcasoundApp"
-              className={FooterStyles.icon}
-            >
-              <TwitterIcon fontSize="large" />
-            </a>
-            <a
-              href="https://www.instagram.com/orcasoundapp/"
-              className={FooterStyles.icon}
-            >
-              <InstagramIcon fontSize="large" />
-            </a>
-          </div>
+          {sendFeedbackLink}
+          {blogLink}
+          {iconContainer}
           <Button
             variant="contained"
             sx={{
