a0833cc6a95cbd52b7ab080f8a77a37f826b2f82
Paul Cretu <paul.s.cretu@gmail.com>
2022-01-30 23:43:41 -0600

    Replace `makeStyles` with `sx` and `styled` (#35)
    * Replace `makeStyles` with `sx` and `styled`
    
    * Remove `@mui/styles` package

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,6 @@ import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import { makeStyles } from '@mui/styles'
 import Image from 'next/image'
 import Link from 'next/link'
 import * as React from 'react'
@@ -39,38 +38,8 @@ const navLinks = [
   },
 ]
 
-const useStyles = makeStyles({
-  nav: {
-    backgroundColor: '#080d26',
-  },
-  navLogoUrl: {
-    width: '60px',
-    height: '44px',
-    top: '20px',
-    left: '15px',
-    borderRadius: '100px',
-    position: 'absolute',
-  },
-  logo: { flexGrow: 1 },
-  logoContainer: {
-    backgroundColor: 'white',
-    width: '69px',
-    height: '66px',
-    margin: '10px',
-    borderRadius: '100px',
-  },
-  link: {
-    color: 'white',
-    '&:hover': {
-      textDecoration: 'none',
-      color: 'white',
-    },
-  },
-})
-
 const Nav = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null)
-  const classes = useStyles()
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget)
@@ -81,18 +50,36 @@ const Nav = () => {
   }
 
   return (
-    <AppBar position="static" className={classes.nav}>
+    <AppBar position="static" sx={{ backgroundColor: '#080d26' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <div className={classes.logo}>
-            <div className={classes.logoContainer}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Box
+              sx={{
+                backgroundColor: 'white',
+                width: '69px',
+                height: '66px',
+                margin: '10px',
+                borderRadius: '100px',
+              }}
+            >
               <Link href="/">
-                <a className={classes.navLogoUrl}>
+                <Box
+                  component="a"
+                  sx={{
+                    width: '60px',
+                    height: '44px',
+                    top: '20px',
+                    left: '15px',
+                    borderRadius: '100px',
+                    position: 'absolute',
+                  }}
+                >
                   <Image src={orcasoundlogo} width={90} height={70} />
-                </a>
+                </Box>
               </Link>
-            </div>
-          </div>
+            </Box>
+          </Box>
 
           <Box sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 0.1 }}>
             {navLinks.map((navLink) => (
@@ -110,7 +97,13 @@ const Nav = () => {
                   <Typography
                     variant="h6"
                     component="a"
-                    className={classes.link}
+                    sx={{
+                      color: 'white',
+                      '&:hover': {
+                        textDecoration: 'none',
+                        color: 'white',
+                      },
+                    }}
                   >
                     {navLink.name}
                   </Typography>
