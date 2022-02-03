30b7c4e09ad3beef597d064107269e817f7930c5
Rodrick <rodrickwamala@gmail.com>
2022-02-03 21:15:31 +0300

    Adds blog-items to the blog-page

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,18 +1,54 @@
 import Image from 'next/image'
 
 import chevronDown from '../../public/icons/chevron-down.svg'
+import frequency1 from '../../public/images/frequency.png'
+import BlogItem from '../components/blog-item'
 import blogStyles from '../styles/blog.module.css'
 
 const blog = () => {
+  const data = [
+    {
+      image: '../../public/images/frequency.png',
+      title: 'Lone first to hear j+K pods entering Puget Sound at Bush Point',
+      summary:
+        'This morning at around 8:30, Lon Brocklehurst texted that he was hearing orcas on the Bush Point. Though it ...',
+      date: '2020-11-11',
+      author: 'Scott Veirs',
+    },
+    {
+      image: '../../public/images/frequency.png',
+      title: 'Election Night humpback calls at Orcasound',
+      summary:
+        'For about a half hour 20:00 this evening (Tues, 3 Nov 2020), Humpback calls were recorded at Orcasound',
+      date: '2020-11-04',
+      author: 'Scott Veirs',
+    },
+  ]
+
   return (
     <>
       <div className={blogStyles.banner}>
         <div className={blogStyles.overlay}>
           <h1 className={blogStyles.bannerText}>Blog</h1>
-          <Image src={chevronDown} alt="chevron-down" />
+          <Image
+            src={chevronDown}
+            alt="chevron-down"
+            className={blogStyles.bannerIcon}
+          />
         </div>
       </div>
-      <section className={blogStyles.blogList}></section>
+      <section className={blogStyles.blogList}>
+        {data.map((item, id) => (
+          <BlogItem
+            key={id}
+            image={item.image}
+            title={item.title}
+            summary={item.summary}
+            author={item.author}
+            datePublished={item.date}
+          />
+        ))}
+      </section>
     </>
   )
 }
