05dca25057d80cb2e15c9efda7e296211aad184f
MOHIT KUMAR SHARMA <mohitkumar.sharma.ece20@itbhu.ac.in>
2022-02-11 15:25:16 +0530

    added about us page content and cards

---
+++ src/components/About/AbouCard.jsx
@@ -0,0 +1,60 @@
+import { Box, Typography } from '@mui/material'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+
+const AboutCard = ({ item, mobileActive, seeMore }) => {
+  return (
+    <Box mb={mobileActive ? 4 : 0} mx={{ xs: 1, sm: 0 }}>
+      <Card
+        sx={{
+          borderRadius: '0px',
+        }}
+      >
+        <CardMedia
+          component="img"
+          alt={item.title}
+          height={'170'}
+          image={item.path}
+          sx={{
+            userSelect: 'none',
+          }}
+        />
+        <CardContent
+          sx={{
+            backgroundColor: '#080d26',
+            color: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100px',
+            width: '100%',
+          }}
+        >
+          <Typography
+            gutterBottom
+            variant="h7"
+            component="div"
+            align="center"
+            mt={2}
+            sx={{
+              fontFamily: 'Montserrat',
+              fontWeight: '500',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '20',
+              width: '100%',
+            }}
+          >
+            {item.title}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  )
+}
+
+export default AboutCard
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,21 @@
+import { Box, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
-import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 
 import orca from '../../public/images/orca2.png'
+import AboutCard from '../components/About/AbouCard'
+import Items from '../components/About/db.json'
 import aboutStyles from '../styles/About.module.css'
 
 export const About = () => {
+  const mobileActive = useMediaQuery('(max-width:600px)')
+  const [seeMore, setseeMore] = useState(mobileActive)
+
   return (
     <div className={aboutStyles.about}>
       <Head>
@@ -20,6 +26,7 @@ export const About = () => {
         src={orca}
         width={1400}
         height={500}
+        alt="landing img"
       />
       <h2 className={aboutStyles.landingText}> What is Orcasound</h2>
 
@@ -29,55 +36,158 @@ export const About = () => {
         bioacustics(AI Technology)
       </p>
 
-      <p className={aboutStyles.introduction}>
-        Orcasound is a coperative effort of many dedicated individuals and great
-        organizations.Here are our recent projects-- created by
-        volunteers,stewards,citizens,scientist,hackers and generous funders--
-        all working together for the orcas
-      </p>
+      <Box
+        sx={{
+          fontFamily: 'Mukta',
+          margin: '10px',
+        }}
+      >
+        <Container>
+          <Typography
+            mt={9}
+            align="justify"
+            variant="body1"
+            sx={{
+              fontFamily: 'Montserrat',
+            }}
+          >
+            Orcasound is a cooperative effort of many dedicated individuals and
+            great organizations. Here are our recent projects — created by
+            volunteers, stewards, citizen scientists, hackers, and generous
+            funders — all working together for the orcas.
+          </Typography>
 
-      <h2 className={aboutStyles.projects}>Our Projects</h2>
-      <div className={aboutStyles.card}>
-        <Card sx={{ maxWidth: 345, mb: 2 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://www.aquariumcostadealmeria.com/wp-content/uploads/2019/01/shutterstock_554899423-1-1280x720.jpg"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Orcasound App
+          <Typography
+            mt={8}
+            mb={5}
+            variant="h4"
+            sx={{
+              fontFamily: 'Mukta',
+              fontWeight: '600',
+            }}
+          >
+            Our Projects
+          </Typography>
+
+          <Box>
+            <Grid
+              container
+              justifyContent="center"
+              spacing={mobileActive ? 0 : 5}
+            >
+              {Items.map((item) => (
+                <Grid item xs={12} sm={6} md={3} key={item.id}>
+                  {((mobileActive && item.id <= 2) ||
+                    seeMore ||
+                    !mobileActive) && (
+                    <AboutCard
+                      item={item}
+                      mobileActive={mobileActive}
+                      seeMore={seeMore}
+                    />
+                  )}
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+          {mobileActive && (
+            <Typography
+              mt={1}
+              gutterBottom
+              onClick={() => setseeMore(!seeMore)}
+              align="center"
+              sx={{
+                fontFamily: 'Montserrat',
+                fontWeight: '600',
+                textDecorationLine: 'underline',
+                '&:hover': {
+                  cursor: 'pointer',
+                },
+              }}
+            >
+              {seeMore ? 'Show less...' : 'See more...'}
             </Typography>
-          </CardContent>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://www.aquariumcostadealmeria.com/wp-content/uploads/2019/01/shutterstock_554899423-1-1280x720.jpg"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Orcasound App
+          )}
+
+          <Box
+            mx={{ xs: 1, sm: 10, md: 20, lg: 40 }}
+            my={13}
+            sx={{
+              fontFamily: 'Mukta',
+            }}
+          >
+            <Typography
+              mt={1}
+              mx={1}
+              align="center"
+              variant="h5"
+              sx={{
+                fontFamily: 'Mukta',
+                fontWeight: '600',
+              }}
+            >
+              We Welcome Your Participation!
             </Typography>
-          </CardContent>
-        </Card>
-      </div>
-      <h2 className={aboutStyles.particiation}>
-        <strong>We Welcome Your Participation</strong>
-      </h2>
-      <p className={aboutStyles.join}>
-        You can join us anytime as a volunteer to our open-source software &
-        hardware projects.
-        <br></br>
-        <br></br>
-        If you'd like to host a hydrophone, do research, or incorporate
-        Orcasound into the educational or outreach efforts of your
-        organization,please reach out!
-      </p>
-      <div className={aboutStyles.button}>
-        <Button variant="outline-primary"> GET INVOLVED!</Button>
-      </div>
+
+            <Box my={3}>
+              <Typography
+                mx={1}
+                mt={3}
+                align="justify"
+                variant="body1"
+                gutterBottom
+                sx={{
+                  fontFamily: 'Montserrat',
+                }}
+              >
+                You can join us anytime as a volunteer to our open-source
+                software & hardware projects.
+              </Typography>
+              <Typography
+                mx={1}
+                mt={3}
+                align="justify"
+                variant="body1"
+                sx={{
+                  fontFamily: 'Montserrat',
+                }}
+                gutterBottom
+              >
+                If you'd like to host a hydrophone, do research, or incorporate
+                Orcasound into the educational or outreach efforts of your
+                organization,please reach out!
+              </Typography>
+            </Box>
+
+            <Box
+              my={3}
+              mt={4}
+              mx={1}
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Button
+                variant="contained"
+                sx={{
+                  background: '#1B2B7B',
+                  alignContent: 'center',
+                  borderRadius: '30px',
+                  '&:hover': {
+                    background: '#1B2B7B',
+                    color: 'white',
+                  },
+                }}
+                href="#contained-buttons"
+              >
+                GET INVOLVED
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
     </div>
   )
 }
