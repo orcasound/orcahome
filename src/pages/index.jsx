3412cf0db186b4da0a57b8f248b13842a7cb2789
alisonqiu <mythpuzzle@gmail.com>
2022-02-23 19:15:11 -0600

    homepage user story

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,10 @@
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
-import {
-  ThemeProvider,
-  createTheme,
-  experimental_sx as sx,
-} from '@mui/material/styles';
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -18,15 +13,13 @@ import arrow from '../../public/images/Arrow.png'
 import vector from '../../public/images/vector.png'
 import homeStyles from '../styles/Home.module.css'
 
-const StyledTypography = styled(Typography)({
-  color: 'white',
-  marginRight: '32px',
-  '&:hover': {
-    textDecoration: 'none',
-    color: 'white',
-    cursor: 'pointer',
-  },
-})
+
+const styles = ({
+  gridContainer: {
+    paddingLeft: "40px",
+    paddingRight: "40px"
+  }
+});
 
 
 export const index = () => {
@@ -45,28 +38,33 @@ export const index = () => {
         />
       </Head>
 
-
       <div className={homeStyles.landing}>
         <Image className={homeStyles.landingImage} src={orcas} />
         <div className={homeStyles.vector}>
           <Image src={vector} />
         </div>
         <div className={homeStyles.flex}>
-          <span>
+          <div>
             <h1 className={homeStyles.landingText}>LISTEN TO ORCAS LIVE!</h1>{' '}
-          </span>
-          <span className={homeStyles.landingArrow}>
+          </div>
+          <div className={homeStyles.landingArrow}>
             <Image src={arrow} width="30px" height="30px" />
-          </span>
+          </div>
         </div>
       </div>
 
-      <div className={homeStyles.home}>
+    
+
+    <div className={homeStyles.home}>
       <body>
-      <h2 className={homeStyles.landingIntro}> What is Orcasound</h2>
-      <br></br>
-      <p className={homeStyles.paragraph}>
-      Orcasound connects your headphones to live hydrophones (underwater
+      
+      <Box component={Grid} item sx={{ml:2, mt:{xs:-18, sm:-15}, mb:{xs:3, sm:8}}} >
+            <Typography variant= 'h1' fontFamily={"Mukta"} fontSize = '44px' fontWeight={'600'} mb={{ xs: 3, sm: 4 }} mr={{xs: 10}} mt={{xs:3}}>
+            What is Orcasound
+            <br></br>
+            </Typography>
+            <Typography variant="body1" fontFamily={'Montserrat'} fontSize = '20px' fontWeight={'600'} lineHeight = '140%'>
+            Orcasound connects your headphones to live hydrophones (underwater
             microphones), your ears to an ocean of sound.
             <br></br>
             <br></br>
@@ -74,63 +72,79 @@ export const index = () => {
             starting with studying and saving the southern resident killer
             whales of the Pacific Northwest. At Orcasound you can listen for
             whales or learn more about marine bioacoustics.
-      </p>
+              </Typography>
+        </Box>
 
-      <div className={homeStyles.card}>
-        <Grid>
-        <Card sx={{ maxWidth: 527, marginBottom: '20px' }}>
-          <CardMedia
-            component="img"
-            height="527"
-            image="https://i2.wp.com/www.orcasound.net/wp2017/wp-content/uploads/2020/10/Screen-Shot-2020-10-20-at-1.40.28-PM.png?resize=660%2C725&ssl=1"
-          />
-          <CardContent sx={{fontFamily: 'Montserrat',fontSize:'20px' }}>
-            <Typography gutterBottom variant="h5" component="div">
-            Current Orcasound hydrophone locations
+      <Grid container spacing = {5} sx={{}}>
+        <Box component={Grid} item display={{ xs: "block", sm: "none" }} sx={{ml:2, fontFamily: 'Montserrat'}}>
+            <Typography variant= 'h1' fontFamily={"Mukta"} fontSize = '44px' fontWeight={'600'} mr={{xs: 10}}>
+                Hydrophone Location
             </Typography>
-          </CardContent>
-        </Card>
-        <Card
-              md={{ order: 2 }}
-              xs={{ order: 1 }}
-            >
-              <CardContent>
-              <Typography variant="h1">
+            <Typography variant="body" fontSize = '20px' fontWeight={'600'}>
+              <br></br>
+                Orcasound is a cooperative hydrophne network and an
+                open-source software & hardware project.
+              </Typography>
+        </Box>
+
+          <Grid item xs={12} sm={6} sx={{fontFamily: 'Montserrat'}}>
+          <Card style={{ border: "none", boxShadow: "none" }}>
+            <CardMedia
+              component="img"
+              height={'width'}
+              image="https://i2.wp.com/www.orcasound.net/wp2017/wp-content/uploads/2020/10/Screen-Shot-2020-10-20-at-1.40.28-PM.png?resize=770%2C725&ssl=1"
+            />
+            <CardContent sx={{fontSize:'20px'}}>
+              <Typography gutterBottom component="p" textAlign= "center" fontFamily= 'Montserrat' fontSize = '18px' fontWeight={'400'} fontStyle = {'normal'}>
+              Current Orcasound hydrophone locations
+              </Typography>
+            </CardContent>
+          </Card>
+          </Grid>
+
+          <Box component={Grid} item display={{ xs: "block", sm: "none" }} sx={{ml:2}}>
+                <Typography fontFamily = 'Montserrat' fontSize = '20px' fontWeight={'600'} mb={{ xs: 3 }} mt={{ xs: -5 }}>
+                  We Welcome your participation. If you'd like to host a
+                  hydrophone, do research or incorporate Orcasound into the
+                  educational or research efforts of your organization , you
+                  can join us as a member of the network.
+                </Typography>
+                <Button variant="contained"
+                  className={homeStyles.button}
+
+             > LEARN</Button>
+                  <Button variant="contained"
+                  className={homeStyles.button}
+               > GET INVOLVED</Button>
+          </Box>
+          
+          <Box component={Grid} item sm={6} display={{ xs: "none", sm: "block" }}>
+              <Typography variant= 'h1' fontFamily={"Mukta"} fontSize = '44px' fontWeight={'600'} >
                   Hydrophone Location
               </Typography>
-              <Typography variant="body">
+              <Typography variant="body" fontFamily = 'Montserrat' fontSize = '20px' fontWeight={'600'}>
                 <br></br>
                   Orcasound is a cooperative hydrophne network and an
                   open-source software & hardware project.
+                  <br></br>
+                  <br></br>
                 </Typography>
-              </CardContent>
-        </Card>
-        <Card
-          md={{ order: 3 }}
-          xs={{ order: 3 }}
-        >
-            <CardContent>
-              <Typography>
-                We Welcome your participation. If you'd like to host a
-                hydrophone, do research or incorporate Orcasound into the
-                educational or research efforts of your organization , you
-                can join us as a member of the network.
-              </Typography>
-              </CardContent>
-              <CardActions>
-                <br></br>
-                <Button>
-                  {' '}
-                  LEARN
-                </Button>{' '}
-                <Button>
-                  {' '}
-                  GET INVOLVED!
-                </Button>{' '}
-            </CardActions>
-        </Card>
-        </Grid>
-      </div>
+                <Typography fontFamily = 'Montserrat' fontSize = '20px' fontWeight={'600'} mb={5}>
+                  We Welcome your participation. If you'd like to host a
+                  hydrophone, do research or incorporate Orcasound into the
+                  educational or research efforts of your organization , you
+                  can join us as a member of the network.
+                </Typography>
+                <Button variant="contained"
+                  className={homeStyles.button}
+             > LEARN</Button>
+                  <Button variant="contained"
+                  className={homeStyles.button}
+               > GET INVOLVED</Button>
+          </Box>
+          
+      </Grid>
+
       {/* Netlify badge for open source plan https://www.netlify.com/legal/open-source-policy */}
       <a href="https://netlify.com">
         <Image
