3856d58e5d3c9327b2e6163a582d0431526d4f55
evanjscallan <evanjscallan@gmail.com>
2022-03-11 14:38:22 -0600

    Added module declaration for TypeScript to allow custom color additions.

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,9 @@
 import Button from '@mui/material/Button'
 
 // ActionButton receives `link` and `text` as props
-// `text` receives the text you want to place inside of the button
-// `link` determines the href that the user is directed to after clicking the button
+// `text` receives the text you want to place inside of the button.
+// `link` determines the href that the user is directed to after clicking the button.
+// 'accent1' is a custom variable declared in theme.ts through a custom value and module delcaration.
 const ActionButton = ({ link, text }) => {
   return (
     <div>
@@ -26,7 +27,4 @@ const ActionButton = ({ link, text }) => {
     </div>
   )
 }
-
 export default ActionButton
-
-/**/
