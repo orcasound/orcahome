b95909d37c37906fa65c56c6884ddd9950c563b0
alisonqiu <mythpuzzle@gmail.com>
2022-05-04 19:39:53 -0500

    5/4

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,16 @@
 import { red } from '@mui/material/colors'
 import { createTheme } from '@mui/material/styles'
 
+//Augmentation allows for custom color additions to theme (TypeScript specific)
+declare module '@mui/material/styles' {
+  interface Palette {
+    accent1: Palette['primary']
+  }
+  interface PaletteOptions {
+    accent1: PaletteOptions['primary']
+  }
+}
+
 // Create a theme instance.
 const theme = createTheme({
   palette: {
@@ -8,10 +18,34 @@ const theme = createTheme({
       main: '#556cd6',
     },
     secondary: {
-      main: '#19857b',
+      main: '#F79234',
     },
     error: {
       main: red.A400,
+    },
+    accent1: {
+      main: '#007bff',
+    },
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#080d26',
+        },
+      },
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 780,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
     },
   },
 })
