2fb864ae2ff4669f152a9f23323a0ff148585282
JurgenYu <tigeryhw@gmail.com>
2023-11-06 22:31:25 -0800

    fix: adjust latout and style

--- src/components/Donate/DonateOrcasound.tsx
+++ src/components/Donate/DonateOrcasound.tsx
@@ -10,27 +10,79 @@ interface DonateOrcasoundProps {
 }
 
 const DonateContainer = styled(Box)(({ theme }) => ({
+  margin: '2vw',
+  padding: '3vw',
   border: 'solid black',
   width: '45vw',
 }))
 
+const ImageContainer = styled(Box)(({ theme }) => ({
+  borderRadius: '1em',
+  overflow: 'hidden',
+}))
+
 const DonateOrcasound = (props: DonateOrcasoundProps) => {
   return (
-    <DonateContainer>
-      <Image src={props.donateOrcasoundImage} alt="" />
-      <Typography>{props.donateOrcasoundMessage}</Typography>
-      <Button
-        variant="contained"
-        sx={{
-          backgroundColor: '#1B2B7B',
-          borderRadius: '20px',
-          width: 'fit-content',
-        }}
-      >
-        {' '}
-        Donate Now
-      </Button>
-    </DonateContainer>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      <DonateContainer>
+        <ImageContainer>
+          <Image
+            src={props.donateOrcasoundImage}
+            alt="Picture of Donate to Orcasound"
+            quality={100}
+            layout="responsive"
+            objectFit="contain"
+          />
+        </ImageContainer>
+        <Typography sx={{ margin: '10px' }}>
+          {props.donateOrcasoundMessage}
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: '#1B2B7B',
+            borderRadius: '20px',
+            margin: '10px',
+            width: 'fit-content',
+          }}
+        >
+          {' '}
+          Donate Now
+        </Button>
+      </DonateContainer>
+      <DonateContainer>
+        <ImageContainer>
+          <Image
+            src={props.donateVolunteersImage}
+            alt="Picture of Donate to Volunteers"
+            quality={100}
+            layout="responsive"
+            objectFit="contain"
+          />
+        </ImageContainer>
+
+        <Typography sx={{ margin: '10px' }}>
+          {props.donateVolunteersMessage}
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: '#1B2B7B',
+            borderRadius: '20px',
+            margin: '10px',
+            width: 'fit-content',
+          }}
+        >
+          {' '}
+          Donate Now
+        </Button>
+      </DonateContainer>
+    </Box>
   )
 }
 
