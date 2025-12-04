3d29049112d041df6f498c114cc3e57b3a508bbd
Benji <benjifriedman@gmail.com>
2025-12-04 00:52:56 -0800

    Format UserReportAPILayer with Prettier

--- src/utils/UserReportAPILayer.tsx
+++ src/utils/UserReportAPILayer.tsx
@@ -4,11 +4,6 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { LastDetection } from '../components/LastDetection'
 //Feel free to add additional GraphQL queries as needed
 
-
-
-
-
-
 export const UserReportAPILayer = (): JSX.Element => {
   const [date, setDate] = useState<string>('')
 
@@ -35,35 +30,33 @@ export const UserReportAPILayer = (): JSX.Element => {
         pageSize: 10,
       },
     }
-    request(
-      'https://live.orcasound.net/graphql',
-      detectionQuery,
-      variables
-    ).then((data: any): any => {
-      //Reformatting of time data for LastDetection component
-      //Array time data
-      if (
-        data &&
-        data.detections &&
-        Array.isArray(data.detections.entries) &&
-        data.detections.entries.length > 0 &&
-        data.detections.entries[0].timestamp
-      ) {
-        const dateIsolate = data.detections.entries[0].timestamp
-        const replaceChars: string = dateIsolate
-          .replace('Z', '')
-          .replace('T', '-')
-          .replace('/-/g', '/')
-        const splitChars: any = replaceChars.split('-')
-        setDate(splitChars[1] + '-' + splitChars[2] + '-' + splitChars[0])
-      } else {
-        // Handle case where no detection entries are available
-        setDate('No detection data')
-      }
-    }).catch((error: any) => {
-      console.error('Error fetching detections:', error)
-      setDate('Error fetching data')
-    })
+    request('https://live.orcasound.net/graphql', detectionQuery, variables)
+      .then((data: any): any => {
+        //Reformatting of time data for LastDetection component
+        //Array time data
+        if (
+          data &&
+          data.detections &&
+          Array.isArray(data.detections.entries) &&
+          data.detections.entries.length > 0 &&
+          data.detections.entries[0].timestamp
+        ) {
+          const dateIsolate = data.detections.entries[0].timestamp
+          const replaceChars: string = dateIsolate
+            .replace('Z', '')
+            .replace('T', '-')
+            .replace('/-/g', '/')
+          const splitChars: any = replaceChars.split('-')
+          setDate(splitChars[1] + '-' + splitChars[2] + '-' + splitChars[0])
+        } else {
+          // Handle case where no detection entries are available
+          setDate('No detection data')
+        }
+      })
+      .catch((error: any) => {
+        console.error('Error fetching detections:', error)
+        setDate('Error fetching data')
+      })
   }, [])
   return (
     <>
