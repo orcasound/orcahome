877161c8ac2277455d75ac4a91eec54e4a022635
fdsafewa <37294298+fdsafewa@users.noreply.github.com>
2026-02-11 20:21:17 -0800

    Move GTM setup from _app to _document
    Remove client-side gtag Script usage from _app.tsx and add Google Tag Manager snippets to _document.tsx. The head now includes the GTM script via dangerouslySetInnerHTML and the body contains the noscript iframe for GTM-TTS69V9J. Also remove the unused next/script import in _app.tsx. This ensures GTM is injected at the document level (head and body) and supports noscript usage.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,10 +19,32 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          {/* Google Tag Manager */}
+          {/* eslint-disable-next-line react/no-danger */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TTS69V9J');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-TTS69V9J"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
