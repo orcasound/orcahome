66cb2336c03a25d53e3b98fadaf890bc7230f897
Stephen Aranda <aranda.stephen88@gmail.com>
2026-05-12 18:37:54 -0500

    added first paragraph

--- src/pages/contributors.jsx
+++ src/pages/contributors.jsx
@@ -14,9 +14,12 @@ import useIsMobile from '../utils/useIsMobile'
 const contributors = () => {
   return (
     <>
+      {/* title of the page Hacker Hall of Fame */}
       <Head>
         <title>Orcasound | Hacker Hall of Fame</title>
       </Head>
+
+      {/* Banner and Banner Text */}
       <div style={{ whiteSpace: 'pre-wrap' }}>
         <TopBanner
           bannerImg={HackerBanner}
@@ -24,6 +27,8 @@ const contributors = () => {
           scrollToId={'contributors'}
         />
       </div>
+
+      {/* Container for Thank you Message for contributors */}
       <Container maxWidth={false} disableGutters>
         <Box
           sx={{
@@ -38,6 +43,30 @@ const contributors = () => {
         >
           <Typography variant="h3">
             Thank you, Orcasound App Hackers!
+          </Typography>
+        </Box>
+      </Container>
+
+      {/* First Section for this page- top */}
+      <Container maxWidth="md">
+        <Box
+          sx={{
+            height: '10em',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+          }}
+        >
+          <Typography variant="body2">
+            We would like to acknowledge the tremendous contributions of time,
+            technology, and code that have been made to our open source project.
+            Beginning in the fall of 2018, we began participating in hackathons
+            in Seattle and at the University of Washington. In summer, 2019, we
+            partnered with Microsoft through their amazing hackathon community.
+            In 2020, Orcasound became a host organization for Google Summer of
+            Code (GSoC).
           </Typography>
         </Box>
       </Container>
