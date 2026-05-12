bafec9ef18129b0bcc6db00b8ce3e3869fc97fe9
Stephen Aranda <aranda.stephen88@gmail.com>
2026-05-12 16:28:24 -0500

    added state,mobile, and banner imports

--- src/pages/contributors.jsx
+++ src/pages/contributors.jsx
@@ -3,12 +3,19 @@
  * Desc : Page for the Hacker Hall of Fame contributors page.
  *  */
 
+import Head from 'next/head'
+import { useState } from 'react'
+
+import TopBanner from '../components/TopBanner'
+import { pushToDataLayer } from '../utils/gtm'
+import useIsMobile from '../utils/useIsMobile'
+
 const contributors = () => {
   return (
     <>
-      <head>
+      <Head>
         <title>Orcasound | Hacker Hall of Fame</title>
-      </head>
+      </Head>
     </>
   )
 }
