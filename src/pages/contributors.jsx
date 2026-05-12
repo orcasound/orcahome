d3d3e91e156540bb2ed4efd5d4d41bb7f77ea3a3
Stephen Aranda <aranda.stephen88@gmail.com>
2026-05-12 16:20:39 -0500

    added title

--- src/pages/contributors.jsx
+++ src/pages/contributors.jsx
@@ -2,3 +2,15 @@
  * File : contributors.jsx
  * Desc : Page for the Hacker Hall of Fame contributors page.
  *  */
+
+const contributors = () => {
+  return (
+    <>
+      <head>
+        <title>Orcasound | Hacker Hall of Fame</title>
+      </head>
+    </>
+  )
+}
+
+export default contributors
