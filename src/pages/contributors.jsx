b02adf6045a427d9234e062b3c6aa138137112a0
Stephen Aranda <aranda.stephen88@gmail.com>
2026-05-12 18:17:50 -0500

    added container and box for thank you msg

--- src/pages/contributors.jsx
+++ src/pages/contributors.jsx
@@ -3,6 +3,7 @@
  * Desc : Page for the Hacker Hall of Fame contributors page.
  *  */
 
+import { Box, Container, Link, Typography } from '@mui/material'
 import Head from 'next/head'
 
 import HackerBanner from '../../public/images/Hacker_HOF.webp'
@@ -23,6 +24,23 @@ const contributors = () => {
           scrollToId={'contributors'}
         />
       </div>
+      <Container maxWidth={false} disableGutters>
+        <Box
+          sx={{
+            height: '5em',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'blue',
+            color: 'white',
+          }}
+        >
+          <Typography variant="h3">
+            Thank you, Orcasound App Hackers!
+          </Typography>
+        </Box>
+      </Container>
     </>
   )
 }
