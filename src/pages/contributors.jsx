e782cce7da1fa3903da4b1f074a44c3280b42ace
Stephen Aranda <aranda.stephen88@gmail.com>
2026-05-14 19:16:46 -0500

    added google header

--- src/pages/contributors.jsx
+++ src/pages/contributors.jsx
@@ -689,30 +689,49 @@ const contributors = () => {
         {/*End of founder contributors list */}
 
         {/* Beginning of the influencers list */}
-        <Container maxWidth="xl">
-          {/* Influencer's box */}
-          <Box
-            sx={{
-              height: '10em',
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              color: 'white',
-              backgroundColor: 'navy',
-              borderRadius: '20px',
-              boxSizing: 'border-box',
-              boxShadow: '10px 10px 5px 2px rgba(0,0,0,0.23)',
-            }}
-          >
-            <Typography variant="h4" gutterBottom>
-              Influencers
-            </Typography>
-            <Typography variant="body2">(Major Contributors)</Typography>
-          </Box>
-        </Container>
+
+        {/* Influencer's box */}
+        <Box
+          sx={{
+            height: '10em',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+            backgroundColor: 'navy',
+            borderRadius: '20px',
+            boxSizing: 'border-box',
+            boxShadow: '10px 10px 5px 2px rgba(0,0,0,0.23)',
+          }}
+        >
+          <Typography variant="h4" gutterBottom>
+            Influencers
+          </Typography>
+          <Typography variant="body2">(Major Contributors)</Typography>
+        </Box>
       </Container>
+
+      {/* google participant header */}
+      <Box
+        sx={{
+          height: '10em',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{ display: 'flex', alignItems: 'center' }}
+        >
+          Google Summer of Code (GSoc) Participants
+        </Typography>
+      </Box>
     </>
   )
 }
