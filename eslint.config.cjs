68e3fcbc968359e13e4a6f9cb89796ffc65ec548
Matt Beaudoin <ehxtwo@gmail.com>
2026-05-20 12:18:17 -0700

    lint on 9.39.4, running

---
+++ eslint.config.cjs
@@ -0,0 +1,77 @@
+const nextPlugin = require('@next/eslint-plugin-next')
+const reactPlugin = require('eslint-plugin-react')
+const reactHooksPlugin = require('eslint-plugin-react-hooks')
+const simpleImportSort = require('eslint-plugin-simple-import-sort')
+const tsParser = require('@typescript-eslint/parser')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+
+module.exports = [
+  {
+    ignores: ['**/.next/**', '**/node_modules/**'],
+  },
+
+  // JS/JSX/TS/TSX base config
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      // Next.js recommended (without core-web-vitals)
+      ...nextPlugin.configs.recommended.rules,
+
+      // React
+      ...reactPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // not needed in Next.js
+      'react/prop-types': 'off', // not needed with TypeScript
+
+      // React hooks
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react-hooks/set-state-in-effect': 'warn', // TODO: remove this warning and fix the set state hooks
+
+      // Import sorting
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+
+      // General
+      'prefer-const': 'warn',
+    },
+  },
+
+  // TS/TSX additional config
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]
