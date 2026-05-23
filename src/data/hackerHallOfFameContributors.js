666172ea9647f902f5dc86d95ae698c1331dfaf9
Stephen Aranda <aranda.stephen88@gmail.com>
2026-05-22 22:30:02 -0500

    added populate and non populated link props

--- src/data/hackerHallOfFameContributors.js
+++ src/data/hackerHallOfFameContributors.js
@@ -17,7 +17,7 @@ export const contributors = [
   {
     name: 'Skander Mzali',
     roles: ['Orcasound site (2017-2020)', 'Admin UI (2019)'],
-    link: '',
+    link: 'https://github.com/skanderm',
   },
   {
     name: 'Steve Hicks',
@@ -35,7 +35,7 @@ export const contributors = [
       'Node engineering and machine learning (2017+)',
       'GSoC mentor (2020+)',
     ],
-    link: '',
+    link: 'https://github.com/orcasound',
   },
   {
     name: 'Scott Veirs',
@@ -53,7 +53,7 @@ export const contributors = [
       '2017 Kickstarter and design guidance (2017+)',
       'GSoC mentor (2021)',
     ],
-    link: '',
+    link: 'https://www.linkedin.com/in/tylercris',
   },
   {
     name: 'Christian Sarason',
@@ -113,56 +113,67 @@ export const googleContributors = [
     name: 'Kunal Mehta',
     country: '(India)',
     roles: ['GSoC contributor (2020)', 'GSoC mentor (2021)'],
+    link: '',
   },
   {
     name: 'Diego Roderiguez',
     country: '(Mexico)',
     roles: ['GSoC contributor (2020)'],
+    link: '',
   },
   {
     name: 'Isabella Macchiavello',
     country: '(Ecuador)',
     roles: ['GSoC contributor (2021)'],
+    link: '',
   },
   {
     name: 'Dmitry Volodin',
     country: '(Russia)',
     roles: ['GSoC contributor (2021)'],
+    link: '',
   },
   {
     name: 'Dhananjay Purohit',
     country: '(India)',
     roles: ['GSoC contributor (2021)'],
+    link: '',
   },
   {
     name: 'Jose Giraldo',
     country: '(Colombia, Spain)',
     roles: ['GSoC contributor (2021)'],
+    link: '',
   },
   {
     name: 'Ambra Jin',
     country: '(Italy, UK, Germany)',
     roles: ['GSoC contributor (2022)'],
+    link: '',
   },
   {
     name: 'Devdoot Chatterjee',
     country: '(India)',
     roles: ['GSoC contributor (2022)'],
+    link: '',
   },
   {
     name: 'Benjamin Chew',
     country: '(Singapore, USA)',
     roles: ['GSoC contributor (2022)'],
+    link: '',
   },
   {
     name: 'Karan Mishra',
     country: '(India)',
     roles: ['GSoC contributor (2022)'],
+    link: '',
   },
   {
     name: 'Paul Nguyen Hong Duc',
     country: '(France, Canada)',
     roles: ['GSoC mentor (2022)'],
+    link: '',
   },
 ]
 
@@ -171,38 +182,47 @@ export const liveContributors = [
   {
     name: 'Zarema Ross',
     roles: ['UX/UI/design project manager (2021+)'],
+    link: '',
   },
   {
     name: 'Elena Bukhonok',
     roles: ['UX/UI/design project manager (2020-21)'],
+    link: '',
   },
   {
     name: 'Sam St Michael',
     roles: ['Design, branding, social media (2020+)'],
+    link: '',
   },
   {
     name: 'James Pedersen',
     roles: ['Full stack developer (2020)'],
+    link: '',
   },
   {
     name: 'Michelle Bocklage',
     roles: [],
+    link: 'https://www.linkedin.com/in/michellebocklage/',
   },
   {
     name: 'Kristen Gillaspy',
     roles: [],
+    link: 'https://kristengillaspyux.com/',
   },
   {
     name: 'Wei Li',
     roles: [],
+    link: '',
   },
   {
     name: 'Catherine Zeng',
     roles: [],
+    link: '',
   },
   {
     name: 'Carina Cheng',
     roles: [],
+    link: '',
   },
 ]
 
@@ -211,26 +231,32 @@ export const arcatiaContributors = [
   {
     name: 'Maria Palamar',
     roles: ['Facilitation & user research (2020+)'],
+    link: '',
   },
   {
     name: 'Graise Lee Jenni',
     roles: ['Facilitation & user research (2020+)'],
+    link: '',
   },
   {
     name: 'Ali Alaydrus',
     roles: ['Development (2020; 2022-3)'],
+    link: '',
   },
   {
     name: 'Peter Ince',
     roles: ['Development (2020-22)'],
+    link: '',
   },
   {
     name: 'Nick Bryne',
     roles: ['Design & Project Management (2020-21)'],
+    link: '',
   },
   {
     name: 'Virgil Zetterlind',
     roles: ['Data Exchange + Mobile App Customization (2019+)'],
+    link: '',
   },
 ]
 
@@ -239,18 +265,22 @@ export const projectManagementContributors = [
   {
     name: 'Firuze Gokce',
     roles: ['Project Manager, Fundraising (2022-23)'],
+    link: '',
   },
   {
     name: 'Heather Gordon',
     roles: ['Project Manager (2021-2)'],
+    link: '',
   },
   {
     name: 'Sanjari Jain',
     roles: ['Project Manager (2021)'],
+    link: '',
   },
   {
     name: 'Garima Chandra',
     roles: ['Development Project Manager (2021)'],
+    link: '',
   },
 ]
 
@@ -259,34 +289,42 @@ export const orcaContributors = [
   {
     name: 'Prakruti Gogia',
     roles: ['Overall Lead'],
+    link: '',
   },
   {
     name: 'Mike Cowan',
     roles: ['Moderation system, Front End'],
+    link: '',
   },
   {
     name: 'Claire Goetschel',
     roles: ['Moderation system, Design'],
+    link: '',
   },
   {
     name: 'Akash Mahajan',
     roles: ['Annotation System'],
+    link: '',
   },
   {
     name: 'Aayush Agrawal',
     roles: ['Machine Learning Development'],
+    link: '',
   },
   {
     name: 'Chris Hanke',
     roles: ['Public Relations'],
+    link: '',
   },
   {
     name: 'Adele Bai',
     roles: ['Notification System'],
+    link: '',
   },
   {
     name: 'Michelle Yang',
     roles: ['Azure Administration+'],
+    link: '',
   },
 ]
 
@@ -295,49 +333,61 @@ export const individualContributors = [
   {
     name: 'Erin',
     roles: ['User-centered design of 2019 UI v2 (2019)'],
+    link: '',
   },
   {
     name: 'Shawn Vita',
     roles: ['Implementation of 2019 UI v2 (2019)'],
+    link: '',
   },
   {
     name: 'Samantha Berk',
     roles: ['Administrative back end (2018-19)'],
+    link: '',
   },
   {
     name: 'Nóra Mészáros',
     roles: ['Graphic design (2017-2020)'],
+    link: '',
   },
   {
     name: 'Erika Pelaez',
     roles: ['Machine learning (2018)'],
+    link: '',
   },
   {
     name: 'Jennifer Rogers',
     roles: ['Machine learning & spectrograms from FLAC files (2018+)'],
+    link: '',
   },
   {
     name: 'Ivan Storck',
     roles: ['Orcamap code clean-up & contribution standards (2020-21)'],
+    link: '',
   },
   {
     name: 'Joyce Liao',
     roles: ['Orcanode repo & Raspberry Pi testing (2020+)'],
+    link: '',
   },
   {
     name: 'Adrian MacDonald',
     roles: ['Content strategy, analytics, conservation impact (2021+)'],
+    link: '',
   },
   {
     name: 'Maria Palamar',
     roles: ['Conservation impact (2020+)'],
+    link: '',
   },
   {
     name: 'Glenn Block',
     roles: [],
+    link: '',
   },
   {
     name: 'Amie Dabu',
     roles: [],
+    link: 'https://www.linkedin.com/in/amiedabu/',
   },
 ]
