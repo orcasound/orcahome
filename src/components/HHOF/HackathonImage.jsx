1871372631dda4b89eff6324e5d3ed4ed24ce24c
Stephen Aranda <aranda.stephen88@gmail.com>
2026-06-03 20:02:32 -0500

    added stubbed component

--- src/components/HHOF/HackathonImage.jsx
+++ src/components/HHOF/HackathonImage.jsx
@@ -4,3 +4,14 @@
  * Desc  : This component is specifically for the hackathon image in the top section of HHOF page
  *
  */
+
+import { Box, Container, Typography } from '@mui/material'
+import Image from 'next/image'
+
+import hackathon from '../../../public/images/getinvolved/hackathon.png'
+
+const HackathonImage = () => {
+  return <div className="hackathon-image-container"></div>
+}
+
+export default HackathonImage
