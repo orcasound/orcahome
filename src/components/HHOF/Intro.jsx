ad6a80021359b92813200f404a301a3846163a85
Stephen Aranda <aranda.stephen88@gmail.com>
2026-06-03 19:56:35 -0500

    added component to render intro paragraphs

--- src/components/HHOF/Intro.jsx
+++ src/components/HHOF/Intro.jsx
@@ -2,7 +2,35 @@
  * Author: Stephen Aranda
  * File  : Intro.jsx
  * Desc  : This component is for the introduction paragraphs that sit in the top section of the page
- *
+ * @param : text: paragraph that will be styled and rendered.
  *  */
 
-import { Box, Container, Link, Typography } from '@mui/material'
+import { Box, Link, Typography } from '@mui/material'
+
+const IntroParagraph = ({ text }) => {
+  return (
+    <div className="intro-paragraph-container">
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'black',
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: '20px',
+            fontWeight: '500',
+            lineHeight: '140%',
+          }}
+        >
+          {text}
+        </Typography>
+      </Box>
+    </div>
+  )
+}
+
+export default IntroParagraph
