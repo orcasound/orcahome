35a7eb91d100176ea2f15cea8e62a4ff51bb5a01
Stephen Aranda <aranda.stephen88@gmail.com>
2026-06-03 20:21:47 -0500

    added stubbed comp

---
+++ src/components/HHOF/ContributorSection.jsx
@@ -0,0 +1,9 @@
+/*
+ * Author: Stephen Aranda
+ * File  : ContributorSection.jsx
+ * Desc  : This component will render a contributor section of the hhof page
+ * */
+
+const ContributorSection = () => {}
+
+export default ContributorSection
